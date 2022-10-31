e9dd07baa4718cb2b9d14c6628655a8d35bbd560
Daniel <officialcodedevelop@gmail.com>
2022-10-31 10:36:45 +0200

    Initialized project
    - Created the base android project
    - Added firebase SDK
    - Added very very base of the activities design

---
+++ app/src/main/java/me/danielml/finalschoolapp/activities/ReportActivity.java
@@ -0,0 +1,16 @@
+package me.danielml.finalschoolapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.danielml.finalschoolapp.R;
+
+public class ReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+    }
+}
