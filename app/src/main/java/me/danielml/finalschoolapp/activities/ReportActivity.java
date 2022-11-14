b5263c3f3ff2a9de7e4561b18450d7fe70e98f1f
Daniel <officialcodedevelop@gmail.com>
2022-11-14 10:30:48 +0200

    Added content the issue type select & custom xml
    - Added spinner_item xml for each item in the XML
    - Added content filling up for the issue type select, with a hashmap to translate it to db values

--- app/src/main/java/me/danielml/finalschoolapp/activities/ReportActivity.java
+++ app/src/main/java/me/danielml/finalschoolapp/activities/ReportActivity.java
@@ -6,20 +6,32 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import me.danielml.finalschoolapp.R;
 import me.danielml.finalschoolapp.objects.Test;
 
-public class ReportActivity extends AppCompatActivity {
+public class ReportActivity extends AppCompatActivity  {
 
     private LinearLayout testView;
+    private Spinner issueTypeSelect;
+    private HashMap<String, String> selectValuesToDbIDs = new HashMap<>();
     private Test test;
+
+    private ArrayAdapter<String> selectAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +47,18 @@ public class ReportActivity extends AppCompatActivity {
             View v = inflater.inflate(R.layout.test_layout, testView);
             buildViewWithoutReportBtn(v);
         }
+        issueTypeSelect = findViewById(R.id.issueTypeSelect);
+
+        selectValuesToDbIDs.put("TWO_TESTS", "רשומים שני מועדים, אבל קיים רק מועד האחד");
+        selectValuesToDbIDs.put("INCORRECT_TEST_TYPE", "רשום סוג אחר של מועד ממה שהוא אמור להיות");
+        selectValuesToDbIDs.put("DOESNT_EXIST", "המבחן הזה לא קיים יותר בלוח מבחנים");
+        selectValuesToDbIDs.put("CLASS_NUMS_WRONG", "הכיתות שקשורות למועד הזה לא נכונים");
+
+        selectAdapter = new ArrayAdapter<String>(this.getBaseContext(), R.layout.spinner_item, new ArrayList<>(selectValuesToDbIDs.values()));
+        selectAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        issueTypeSelect.setAdapter(selectAdapter);
+
+
     }
 
     // I am not proud of having to copy-paste the method here, but there isn't really a viable solution structure-wise
