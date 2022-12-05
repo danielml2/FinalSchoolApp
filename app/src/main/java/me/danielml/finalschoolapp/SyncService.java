9c01ead862eec88a387c069af0f24de7fc430801
Daniel <officialcodedevelop@gmail.com>
2022-12-05 10:17:48 +0200

    Added the base function of syncing tests data

--- app/src/main/java/me/danielml/finalschoolapp/SyncService.java
+++ app/src/main/java/me/danielml/finalschoolapp/SyncService.java
@@ -15,20 +15,41 @@ import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
 
+import org.json.JSONException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import me.danielml.finalschoolapp.managers.FileManager;
+import me.danielml.finalschoolapp.managers.FirebaseManager;
+
 public class SyncService extends Service {
 
     private Looper serviceLooper;
     private Handler syncHandler;
+    private FileManager fileManager;
+    private FirebaseManager firebaseManager;
 
-    public SyncService() {
-    }
+    private Notification notification;
+    private long lastChecked;
+    private boolean running = false;
+    private final long DELAY = 30000;
+
 
     @Override
     public void onCreate() {
         HandlerThread thread = new HandlerThread("SchoolTestsSync", Process.THREAD_PRIORITY_BACKGROUND);
+        fileManager = new FileManager(getApplicationContext().getFilesDir());
+        firebaseManager = new FirebaseManager();
 
         thread.start();
+        try {
+            lastChecked = fileManager.getLastCheck();
+        } catch (FileNotFoundException | JSONException e) {
+            e.printStackTrace();
+        }
         serviceLooper = thread.getLooper();
+
 
         syncHandler = new Handler(serviceLooper);
     }
@@ -37,24 +58,71 @@ public class SyncService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        startForeground(1, staticRunningNotification());
+        notification = staticRunningNotification();
+        startForeground(1, notification);
         Message msg = syncHandler.obtainMessage();
         msg.arg1 = startId;
         syncHandler.sendEmptyMessage(0);
-        syncHandler.post(() -> {
-            int a = 0;
-            while(true) {
+        long timeSinceLastUpdate = System.currentTimeMillis() - lastChecked;
+
+        long customDelay = DELAY - timeSinceLastUpdate;
+        boolean customFirstDelay = timeSinceLastUpdate >= 0 && timeSinceLastUpdate <= DELAY;
+
+        Log.d("SchoolTests", "Time since last update: " + timeSinceLastUpdate);
+        Log.d("SchoolTests", "Custom delay: " + customFirstDelay);
+
+        if(!running) {
+            syncHandler.post(() -> {
+                boolean firstRun = customFirstDelay;
+                running = true;
+            while(running)
+            {
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(firstRun ? customDelay : DELAY);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                Log.d("SchoolTests: Background Syncer", a + "");
-                a++;
+                if(firstRun) firstRun = false;
+                sync();
+
             }
         });
+        }
 
         return START_STICKY;
+    }
+
+    public void sync() {
+        firebaseManager.getLastUpdatedTime((lastUpdatedDB) -> {
+            try {
+                long localLastUpdate = fileManager.getLocalLastUpdated();
+                if(localLastUpdate < lastUpdatedDB) {
+                    Log.d("SchoolTests Sync (Background)", "Local data is out of date, syncing data from database");
+                    firebaseManager.getCurrentTests((tests) -> {
+                        Log.d("SchoolTests Sync (Background)", "Downloaded database data, new tests count: " + tests.size());
+                        try {
+                            Log.d("SchoolTests Sync", "Saving database data locally...");
+                            fileManager.saveDBDataLocally(lastUpdatedDB, tests);
+                            Log.d("SchoolTests Sync (Background)", "Saved database data locally!");
+                        } catch (JSONException | IOException e) {
+                            Log.e("SchoolTests Sync (Background)", "Failed saving local data! Data is not up to date!");
+                            e.printStackTrace();
+                        }
+                    });
+                } else {
+                    Log.d("SchoolTests Sync (Background)", "Local data is up to date!");
+                }
+            } catch (IOException | JSONException e) {
+                Log.e("SchoolTests Sync (Background)", "Failed loading local last updated. Exiting...");
+                e.printStackTrace();
+            }
+        });
+        lastChecked = System.currentTimeMillis();
+        try {
+            fileManager.saveLastCheck(lastChecked);
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
